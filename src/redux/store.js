e0e483de8fbda1ff2938c8ed92964ed8344d92ac
Tetiana Yarova <andronikova11@gmail.com>
2021-09-14 21:10:51 +0300

    Create auth redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,32 +1,36 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
-// import {
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import { contactsReducer } from "./contacts";
+import storage from "redux-persist/lib/storage";
+import authReducer from "./auth/auth-reducer";
 
-import { contactsReducer } from './contacts';
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token"],
+};
 
-const middleware = [
-  ...getDefaultMiddleware({
-    serializableCheck: false,
-  }),
-
-  logger,
-];
-
-const store = configureStore({
+export const store = configureStore({
   reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
     contacts: contactsReducer,
   },
-  middleware,
-  devTools: process.env.NODE_ENV === 'development',
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === "development",
 });
 
-// const persistor = persistStore(store);
-
-export default store;
+export const persistor = persistStore(store);
